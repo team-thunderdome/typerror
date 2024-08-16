4a579a40fb8153b9ed03c188cd68e370a4246f19
Cooper Simpson <cooper@coopersimpson.com>
2024-08-17 08:24:16 +1000

    POST req working

--- server/index.js
+++ server/index.js
@@ -1,8 +1,52 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Score } from "./models/scoreModel.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+// Middleware to parse JSON data
+app.use(express.json());
+
+// Express.js route handler for the root of the server
+// Takes URL path and a callback function as arguments
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("typeerror");
 });
+
+// Route to save a new player score
+app.post("/scores", async (request, response) => {
+  try {
+    if (!request.body.name || !request.body.score) {
+      return response
+        .status(400)
+        .send({ message: "Name and score are required fields" });
+    }
+    // Create a new score object
+    const newScore = {
+      name: request.body.name,
+      score: request.body.score,
+    };
+    // Send the score object to the database
+    const score = await Score.create(newScore);
+    return response.status(201).send(score);
+  } catch (error) {
+    // Respond with an error message
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    // Test connection running on correct port
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
