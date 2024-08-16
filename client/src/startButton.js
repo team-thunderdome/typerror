53c6cae747c302a335e02a64f5f925b15be09e2d
Tom Dodd <tomdodd@192-168-1-106.tpgi.com.au>
2024-08-17 02:45:50 +1000

    button functionality

--- client/src/startButton.js
+++ client/src/startButton.js
@@ -1,10 +1,16 @@
 import './startButton.css'
+import { useNavigate } from 'react-router-dom';
 
 // THIS SHOULD PROBABLY DO SOMETHING > ROUTE GAMINGPAGE
 const StartButton = ({onClick}) => {
+	let navigate = useNavigate();
+	const ChangePath = () => {
+	let path = '/gamingPage'
+	navigate(path)
+	}
 	return (
 	<div>
-	<button className="startButton" onClick={onClick}>
+	<button className="startButton" onClick={ChangePath}>
 	   &gt; start...
 	</button></div>
 	)
