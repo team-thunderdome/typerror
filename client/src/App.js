5da1f3292c3c04bf55866bc73d91bd0ab2ad49a8
Cooper Simpson <cooper@coopersimpson.com>
2024-08-17 20:35:20 +1000

    Add leaderboard (#6)
    * Add basic leaderboard
    
    * changes to .DS_Store file
    
    * Style leaderboard changes
    
    * Add dummy data
    
    * Add json
    
    * Leaderboard pulls json data

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage.js";
 import GamingPage from "./GamingPage.js";
 import ResultsPage from "./ResultsPage.js";
+import LeaderBoard from "./Leaderboard.js";
+
+// we need to route in other pages
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/gamingpage" element={<GamingPage />} />
           <Route path="/results" element={<ResultsPage />} />
+          <Route path="/leaderboard" element={<LeaderBoard />} />
         </Routes>
       </Router>
     </div>
