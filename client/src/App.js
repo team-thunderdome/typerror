c936c23e1b7c20f1c2e3f10ce46399d822ddd0f0
Cooper Simpson <cooper@coopersimpson.com>
2024-08-17 11:11:23 +1000

    Fix errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./LandingPage.js"
-import GamingPage from "./GamingPage.js"
+import LandingPage from "./LandingPage.js";
+import GamingPage from "./GamingPage.js";
 
 // we need to route in other pages
 
 function App() {
   return (
-	<div>
-	<Router>
-	<Routes>
-	<Route path='/' element={<LandingPage />} />
-	<Route path='/gamingpage' element={<GamingPage />} />
-	</Routes>
-	</Router>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/gamingpage" element={<GamingPage />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
 export default App;
--- client/src/startButton.js
+++ client/src/startButton.js
@@ -1,19 +1,20 @@
-import './startButton.css'
-import { useNavigate } from 'react-router-dom';
+import "./startButton.css";
+import { useNavigate } from "react-router-dom";
 
 // THIS SHOULD PROBABLY DO SOMETHING > ROUTE GAMINGPAGE
-const StartButton = ({onClick}) => {
-	let navigate = useNavigate();
-	const ChangePath = () => {
-	let path = '/gamingPage'
-	navigate(path)
-	}
-	return (
-	<div>
-	<button className="startButton" onClick={ChangePath}>
-	   &gt; start...
-	</button></div>
-	)
-}
+const StartButton = ({ onClick }) => {
+  let navigate = useNavigate();
+  const ChangePath = () => {
+    let path = "/gamingPage";
+    navigate(path);
+  };
+  return (
+    <div>
+      <button className="startButton" onClick={ChangePath}>
+        &gt; start...
+      </button>
+    </div>
+  );
+};
 
-export default StartButton
+export default StartButton;
