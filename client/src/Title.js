13a4b617459b5d8c6f837f510bcb95f7eb46940d
Alvaro <alvarow2002@gmail.com>
2024-08-17 16:20:04 +1000

    🚧 title logo

--- client/src/Title.js
+++ client/src/Title.js
@@ -1,11 +1,10 @@
 import "./Title.css";
+import logo from "./assets/logo.svg";
 
 const Title = () => {
   return (
     <div>
-      <div class="mainTitle">
-        <p>typerror</p>
-      </div>
+      <img src={logo} alt="logo" />
       <div class="title">
         <p>type code, race friends, fix erro)rs</p>
       </div>
