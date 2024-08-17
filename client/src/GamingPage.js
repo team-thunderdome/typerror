f3585058525d9f55ebdb89e8669cd01c12776409
dc <dannie.xh.cai@gmail.com>
2024-08-17 21:09:18 +1000

    💄Fix format for the gaming page

--- client/src/GamingPage.js
+++ client/src/GamingPage.js
@@ -3,18 +3,23 @@ import TextBox from "./TextBox.js";
 import Header from "./Header.js";
 import Codebox from "./Codebox.js";
 
+import "./GamingPage.css"
+
 const GamingPage = () => {
   return (
-    <div
-      style={
-        {
-          /* backgroundColor: '#F7E7CB' */
-        }
-      }
-    >
-      <Header />
-      <Codebox />
-      <TextBox />
+    <div id="content-container">
+      <div id="top-menu-bar">
+        <Header />
+      </div>
+      
+      <div id="code-box">
+        <Codebox />
+      </div>
+      
+      <div id="input-text-box">
+        <TextBox />
+      </div>
+      
     </div>
   );
 };
