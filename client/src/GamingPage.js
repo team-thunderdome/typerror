6923e80e259d4c7b92337607d20a224bafc0aa54
aA-86 <88065449+aA-86@users.noreply.github.com>
2024-08-17 23:31:57 +1000

    gaming page functionality (#10)
    * 💄Fix textbox styling
    
    * 🎨 added command prompt json file
    
    * 👔 added code to fetch data from command prompt
    
    * ♻️ fixed dummy prompt numbering
    
    * 👔 added flag for if timer is finished and counter to cycle through prompts
    
    * ⚗️ added on change even to inputs
    
    * 🚧 added code to update current_prompt_index
    
    * 🚚 update names
    
    * 👔 added code to increment the counter
    
    * 🚚 changed names

--- client/src/GamingPage.js
+++ client/src/GamingPage.js
@@ -1,25 +1,54 @@
+import React, { useEffect, useState } from 'react';
+
+import Title from "./InfoTitle.js";
+import logo from "./assets/logo.svg";
 import TextBox from "./TextBox.js";
 import Header from "./Header.js";
 import Codebox from "./Codebox.js";
 import "./GamingPage.css"
 
+const max_prompt_index = 5; /* from 0 indexing i.e., if there are 2 prompts then max_prompt_index is 1 */
+
 const GamingPage = () => {
+  const [data, setData] = useState(null);
+  const [inputValue, setInputValue] = useState(''); 
+  const [currentPromptIndex, setPromptIndex] = useState(0);
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value); 
+    if (data && inputValue === data.items[currentPromptIndex].answer) {
+      if (currentPromptIndex < max_prompt_index) {
+        setPromptIndex(prevIndex => prevIndex + 1); 
+      }
+      setInputValue(''); 
+    }
+  };
+
+  useEffect(() => {
+    fetch('/../CodePrompt.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  if (!data) {
+    return (<div>Loading...</div>)
+  }
+
   return (
     <div id="content-container">
       <div id="top-menu-bar">
         <Header />
       </div>
-      
       <div id="code-box">
-        <Codebox />
+        <Codebox text={data.items[currentPromptIndex].prompt}/>
       </div>
-      
       <div id="input-text-box">
-        <TextBox />
+        <TextBox value={inputValue} onChange={handleInputChange} />
       </div>
-      
     </div>
   );
+  /* route here to go to next page */
 };
 
 export default GamingPage;
