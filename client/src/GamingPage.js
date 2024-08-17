c7084ee10b48f93282fbd899343174aa6c30d6df
Tom Dodd <tomdodd@d-i89-175-176.student.eduroam.uq.edu.au>
2024-08-18 01:52:12 +1000

    skip timer (COOKING)

--- client/src/GamingPage.js
+++ client/src/GamingPage.js
@@ -6,18 +6,24 @@ import TextBox from "./TextBox.js";
 import Header from "./Header.js";
 import Codebox from "./Codebox.js";
 import Timer from "./Timer.js";
+import skip from "./assets/skip.svg"
+import './LeaderboardButton.js'
 import "./GamingPage.css"
+
 
 const GamingPage = () => {
   const [data, setData] = useState(null);
   const [inputValue, setInputValue] = useState(''); 
   const [currentPromptIndex, setPromptIndex] = useState(0);
+  let score = 0;
+  let failed = 0;
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value); 
     if (data && inputValue === data.items[currentPromptIndex].answer) {
       setPromptIndex(Math.floor(Math.random() * data.items.length)); 
-      setInputValue(''); 
+      setInputValue('');
+      score += 1;
     }
   };
 
@@ -39,6 +45,21 @@ const GamingPage = () => {
     return (<div>Loading...</div>)
   }
 
+  function handleClick() {
+    failed += 1;
+    setPromptIndex(Math.floor(Math.random() * data.items.length));
+    console.log(failed)
+  }
+
+  // Call same thing above if clicked
+  const SkipButton = ({ onClick }) => {
+    return (
+      <div>
+        <img class="skip-button" src={skip} onClick={handleClick} style={{"pointer-events": "all"}} />
+      </div>
+    );
+  };
+
   return (
     <div id="content-container">
       <div id="top-menu-bar">
@@ -49,6 +70,12 @@ const GamingPage = () => {
       </div>
       <div id="input-text-box">
         <TextBox value={inputValue} onChange={handleInputChange} />
+      </div>
+      <div id="skip-button-box">
+        <SkipButton />
+      </div>
+      <div id="timer-box">
+        <Timer />
       </div>
     </div>
   );
