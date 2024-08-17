60409b00dcda571c3c86f87c323958fe1fb9fc4f
dc <dannie.xh.cai@gmail.com>
2024-08-17 22:52:27 +1000

    🚧 added code to update current_prompt_index

--- client/src/GamingPage.js
+++ client/src/GamingPage.js
@@ -7,13 +7,27 @@ import Header from "./Header.js";
 import Codebox from "./Codebox.js";
 import "./GamingPage.css"
 
+const max_prompt_index = 2; /* from 0 indexing i.e., if there are 2 prompts then max_prompt_index is 1 */
+
 const GamingPage = () => {
   const [data, setData] = useState(null);
-
   const [inputValue, setInputValue] = useState(''); 
+  const [current_prompt_index, set_prompt_index] = useState(0);
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value); 
+    if (data && inputValue === data.level_1[current_prompt_index].answer) {
+      set_prompt_index(prevIndex => prevIndex + 1); 
+      setInputValue(''); 
+    }
+    // if (data && inputValue === data.level_1[current_prompt_index].answer) {
+    //   if (current_prompt_index <= max_prompt_index) {
+    //     setData("You're all finished!")
+    //   } else {
+    //     set_prompt_index(prevIndex => prevIndex + 1); 
+    //   }
+    //   setInputValue(''); 
+    // }
   };
 
   useEffect(() => {
@@ -27,27 +41,20 @@ const GamingPage = () => {
     return (<div>Loading...</div>)
   }
 
-  var current_prompt_index = 0;
-  var is_correct = false;
-  var timer_finished = false;
-
-  while (true) {
-    return (
-      <div id="content-container">
-        <div id="top-menu-bar">
-          <Header />
-        </div>
-        
-        <div id="code-box">
-          <Codebox text={data.level_1[current_prompt_index].prompt}/>
-        </div>
-        <div id="input-text-box">
-          <TextBox value={inputValue} onChange={handleInputChange} />
-        </div>
+  return (
+    <div id="content-container">
+      <div>Current prompt = {current_prompt_index}</div>
+      <div id="top-menu-bar">
+        <Header />
       </div>
-    );
-  }
-
+      <div id="code-box">
+        <Codebox text={data.level_1[current_prompt_index].prompt}/>
+      </div>
+      <div id="input-text-box">
+        <TextBox value={inputValue} onChange={handleInputChange} />
+      </div>
+    </div>
+  );
   /* route here to go to next page */
 };
 
