2545b4126f51d19ee1063f723042a999a496a0bb
Cooper Simpson <cooper@coopersimpson.com>
2024-08-17 14:36:05 +1000

    Clean up and basic header

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,14 +1,20 @@
-import StartButton from './StartButton.js'
-import Title from './Title.js'
+import StartButton from "./StartButton.js";
+import Title from "./Title.js";
+import Header from "./Header.js";
 
 const LandingPage = () => {
-return (
-	<div style={{
-        /* backgroundColor: '#F7E7CB' */
-        }}>
-        <Title />
-        <StartButton />
-	</div>
-	)
-}
-export default LandingPage
+  return (
+    <div
+      style={
+        {
+          /* backgroundColor: '#F7E7CB' */
+        }
+      }
+    >
+      <Header />
+      <Title />
+      <StartButton />
+    </div>
+  );
+};
+export default LandingPage;
