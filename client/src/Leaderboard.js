ed6a2d9e19b54f26b4e29c29c2dea649381b536f
Cooper Simpson <cooper@coopersimpson.com>
2024-08-17 17:05:06 +1000

    Add basic leaderboard

---
+++ client/src/Leaderboard.js
@@ -0,0 +1,33 @@
+import Header from "./Header.js";
+import "./Leaderboard.css";
+
+const LeaderBoard = () => {
+  return (
+    <div>
+      <Header />
+      <table className="ldrTable"></table>
+      <tr>
+        <th>Rank</th>
+        <th>Username</th>
+        <th>WPM</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>John</td>
+        <td>100</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Bob</td>
+        <td>90</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Joe</td>
+        <td>80</td>
+      </tr>
+    </div>
+  );
+};
+
+export default LeaderBoard;
