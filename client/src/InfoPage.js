4387a58ccb95d8fb7621678cf1a134834cded2d1
Alvaro <alvarow2002@gmail.com>
2024-08-17 16:28:58 +1000

    initial setup

---
+++ client/src/InfoPage.js
@@ -0,0 +1,16 @@
+import Header from "./Header.js";
+
+const InfoPage = () => {
+    return(
+        <div
+            style={
+                {
+                    /* backgroundColor: '#F7E7CB' */
+                }
+            }
+            >
+            </div>
+    );
+};
+
+export default InfoPage;
