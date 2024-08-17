bcc174df65aa6cf612cc97b45ff6c3c36256d2c0
dc <dannie.xh.cai@gmail.com>
2024-08-17 21:45:17 +1000

    🎨 added command prompt json file

--- client/src/GamingPage.js
+++ client/src/GamingPage.js
@@ -1,9 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+import Title from "./Title.js";
+import logo from "./assets/logo.svg";
 import TextBox from "./TextBox.js";
 import Header from "./Header.js";
 import Codebox from "./Codebox.js";
 import "./GamingPage.css"
 
 const GamingPage = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('/../CodePrompt.json')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  if (!data) {
+    return (<div>Loading...</div>)
+  }
+
   return (
     <div id="content-container">
       <div id="top-menu-bar">
